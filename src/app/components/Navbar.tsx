1b9d0145bc1ff9cd13eedbb9a1036867bf9e2a39
Jeremy Bosma <jectaunscripted@gmail.com>
2023-11-15 16:32:59 +0100

    Update Github link in Navbar component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
       <h1 className="logo">Mythic</h1>
       <div className="right-items">
         <a className="nav-a" href="https://discord.gg/58NZ7fFqPy">Discord</a>
-        <a className="nav-a" href="https://github.com/getmythic">Github</a>
+        <a className="nav-a" href="https://github.com/mythicapp">Github</a>
         <a className="button" href="https://discord.gg/58NZ7fFqPy">
           Waitlist
         </a>
