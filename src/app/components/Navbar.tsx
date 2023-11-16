ac076824ff273c919ba7e2860c412d969a608f74
Jeremy Bosma <jectaunscripted@gmail.com>
2023-11-16 22:17:28 +0100

    Update Wiki link in Navbar component

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
       <div className="right-items">
         <a className="nav-a" href="https://github.com/mythicapp">Github</a>
         <a className="nav-a" href="https://discord.gg/58NZ7fFqPy">Discord</a>
-        <a className="nav-a" href="https://github.com/mythicapp">Wiki</a>
+        <a className="nav-a" href="https://github.com/MythicApp/Mythic/wiki/Introduction">Wiki</a>
         <a className="button" href="https://discord.gg/58NZ7fFqPy">
           Join Waitlist
         </a>
