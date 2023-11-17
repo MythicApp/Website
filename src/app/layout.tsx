800277fcbb5f8aef292560d93eb7b004733b7ed3
Jeremy Bosma <jectaunscripted@gmail.com>
2023-11-17 20:52:34 +0100

    Add meta tags to improve SEO and social media sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,25 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="canonical" href="https://getmythic.app" />
         <meta name="theme-color" content="#a046ff" />
+        <meta property="og:url" content="https://getmythic.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Mythic" />
         <meta property="og:image" content="https://getmythic.app/ogimage.png" />
+        <meta property="og:locale" content="en_US" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Mythic" />
+        <meta name="twitter:description" content="Mythic is a game launcher that bridges the gap between windows and mac." />
+        <meta name="twitter:site" content="@mythicapp" />
+        <meta name="twitter:image" content="https://getmythic.app/ogimage.png" />
+        <meta name="author" content="Your Name" />
+        <meta name="keywords" content="keyword1, keyword2, keyword3" />
+        <meta name="referrer" content="no-referrer-when-downgrade" />
+        <meta http-equiv="content-language" content="en-US" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
