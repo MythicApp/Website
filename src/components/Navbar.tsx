f38c121321609d0231d8319c2fb689f4f08b94ed
blackxfiied <41133734+blackxfiied@users.noreply.github.com>
2023-12-11 14:42:16 +0800

    Update Navbar.tsx
    Capitalize and move FAQ in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@ export default function Navbar() {
     <nav>
       <a href="/"><h1 className="logo">Mythic</h1></a>
       <div className="right-items">
-        <a className="nav-a" href="/faq" target="_blank">Faq</a>
         <a className="nav-a" href="/docs" target="_blank">Docs</a>
         <a className="nav-a" href="https://github.com/orgs/MythicApp/projects/2/views/2" target="_blank">Roadmap</a>
+        <a className="nav-a" href="/faq" target="_blank">FAQ</a>
         <a className="nav-a" href="https://github.com/mythicapp" target="_blank">Github</a>
         <a className="nav-a" href="https://discord.gg/58NZ7fFqPy" target="_blank">Discord</a>
         <a className="button navwaitlist" href="/waitlist">
